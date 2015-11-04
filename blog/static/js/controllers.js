a20663dabd35172f967d5fe2861ea771bfc9672c
Simon Ivanšek <simon.ivansek@xlab.si>
2015-11-04 15:59:37 +0100

    Bugfix

--- blog/static/js/controllers.js
+++ blog/static/js/controllers.js
@@ -22,27 +22,16 @@ blogController.controller('IndexController', function($scope, $http){
 blogController.controller('NewPostController', function($scope, $http){
     $scope.formData = {}
     $scope.processForm = function(){
+        $scope.submitted = true
         $http.post('/api/post/', $scope.formData)
             .success(function(data){
                 $scope.is_error = false
-                $scope.submitted = true
                 $scope.status = 'Your post was successfully saved!'
                 $scope.formData = {}
             })
             .error(function(data){
-                $scope.submitted = true
                 $scope.is_error = true
                 $scope.errors = data
             })
     }
 })
-//
-////blogController.directive("remove", function () {
-//    return function (scope, element, attrs) {
-//        element.bind ("mousedown", function () {
-//            scope.remove(element);
-//            scope.$apply();
-//        })
-//    };
-//
-//});
